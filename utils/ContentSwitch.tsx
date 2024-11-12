31b7d4a4bd8f4ea20d73b1ffc471950bca03592d
Hanna-Kai Barstad Golberg <hbar@netlight.com>
2024-11-12 20:48:39 +0100

    use @ instead of .. to avoid refactors with every directory-change

--- utils/ContentSwitch.tsx
+++ utils/ContentSwitch.tsx
@@ -1,15 +1,13 @@
-
-import Quote from "../components/Quote";
-import { Item } from "../types/Item" 
-import Quiz from "../components/Quiz"
-import { FC } from "react";
+import Quiz from '@/components/Quiz';
+import Quote from '@/components/Quote';
+import { Item } from '@/types/Item';
+import { FC } from 'react';
 
 // type ContentComponentBuilder<T extends Item> = (item: T) => JSX.Element;
 // type ComponentMap<T extends Item> = Record<ContentType, ContentComponentBuilder<T>>;
 
-
-export const ContentSwitch: FC<{item: Item}>= ({item}) => {
-  console.log("item", item);
+export const ContentSwitch: FC<{ item: Item }> = ({ item }) => {
+  console.log('item', item);
 
   switch (item.type) {
     // case "text":
@@ -18,11 +16,10 @@ export const ContentSwitch: FC<{item: Item}>= ({item}) => {
     // case "image":
     //   return( <Quote item={item} />)
 
-    case "quiz":
-      return( <Quiz item={item} />)
-      
-      case "quote":
-      return <Quote item={item} />
-  }
-}
+    case 'quiz':
+      return <Quiz item={item} />;
 
+    case 'quote':
+      return <Quote item={item} />;
+  }
+};
