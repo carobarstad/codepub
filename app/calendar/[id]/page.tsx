65cd3ac75c1cc2335dc4f497ac448fc9be8b7dc3
Caroline Barstad <caroline.barstad@netlight.com>
2024-11-20 18:28:15 +0100

    Remove mock data and add image with allowed hostname for image

--- app/calendar/[id]/page.tsx
+++ app/calendar/[id]/page.tsx
@@ -1,42 +1,13 @@
 import { getItemByIndex } from '@/api/quiz';
-import { Item } from '@/types';
 import { ContentSwitch } from '@/utils/ContentSwitch';
 import { notFound } from 'next/navigation';
-
-const mockDay: Item = {
-  type: 'quiz',
-  question: 'spe',
-  solution: 'sd',
-  options: ['a'],
-  // header: 'Today is a great day',
-  // text: 'This is the best quote'
-};
-
-const mockDays: Item[] = [
-  {
-    type: 'quiz',
-    question: 'spe',
-    solution: 'sd',
-    options: ['a'],
-    // header: 'Today is a great day',
-    // text: 'This is the best quote'
-  },
-  {
-    type: 'quote',
-    // question:"spe", solution: "sd", options: ["a", ]
-    header: 'Today is a great day',
-    text: 'This is the best quote',
-  },
-];
 
 const CalendarDayPage = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
   const idAsInt = parseInt(id);
-  const data = mockDays[idAsInt - 1]; // TODO: Hent info fra backend til den id-en
   const calendarData = await getItemByIndex(idAsInt - 1);
 
-  console.log(calendarData);
-  if (!data) {
+  if (!calendarData) {
     notFound();
   }
   return (
--- components/Image.tsx
+++ components/Image.tsx
@@ -2,7 +2,7 @@ import { ImageContent } from '@/types/Item';
 import Image from 'next/image';
 
 const ImageComponent = ({ item }: { item: ImageContent }) => {
-  return <Image alt={item.alt} src={item.src} />;
+  return <Image alt={item.alt} src={item.src} width={500} height={500} />;
 };
 
 export default ImageComponent;
