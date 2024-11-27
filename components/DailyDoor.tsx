2cb39bfc365c397d5c35a5bc43a466467c456982
karb <karoline.berge@netlight.com>
2024-11-27 13:32:35 +0100

    Add hover and active feedback on dailydoor

--- components/DailyDoor.tsx
+++ components/DailyDoor.tsx
@@ -6,7 +6,7 @@ type Props = {
 const DailyDoor = ({ day, allowedToOpen }: Props) => {
   return (
     <div
-      className={`flex border border-none rounded w-32 h-32 items-center justify-center ${allowedToOpen ? 'bg-[#8bbdd3]' : 'bg-slate-700'}`}
+      className={`flex border border-none rounded w-32 h-32 items-center justify-center hover:bg-[#66a1ba] active:bg-[#4f7e92] ${allowedToOpen ? 'bg-[#8bbdd3]' : 'bg-slate-700'}`}
     >
       <h1 className={`text-4xl font-semibold ' ${allowedToOpen ? 'text-white' : 'text-slate-400'}`}>
         {day}
